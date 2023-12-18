5153816bebee3884ee764fdb7796667a7cb898e0
darkshuige_lab <1597590247@qq.com>
2023-12-18 12:51:05 +0800

    second commit

--- test.cpp
+++ test.cpp
@@ -5,12 +5,6 @@
 using namespace std;
 const int maxn=1005;
 const int maxm=maxn*maxn;
-//G为二分图，在G的子图中，任意两条边都没有公共节点，M就是G的匹配
-//交替路，从一个未匹配点出发，一次经过匹配边，非匹配边
-//增广路，从一个未匹配点除法，沿着交替路走，最后走到一个未匹配点
-//那么增广路比交替路多了一条，只要交换增广路和匹配路，那么匹配路就多了一条
-//匈牙利算法：通过不断找增广路来增加匹配边，dfs或bfs实现
-//也可以用dinic算法，建立超级原点和汇点然后边权都是1
 int h[maxn],to[maxm],ne[maxm],idx=1;
 int vis[maxn],match[maxn];
 char g[maxn][maxn];
